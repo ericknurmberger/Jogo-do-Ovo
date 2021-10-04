7b5a739fab323ddd0aa9dcd34dd493ebeea181e9
ericknurmberger <ericknurmbergerc@gmail.com>
2021-10-04 00:13:59 -0300

    GameOver e NextLevel

---
+++ Jogo_do_Ovo/Assets/Assets/Sprites/Scripts/GameOverScreen.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public void Setup()
+    {
+        gameObject.SetActive(true);
+
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+
+}
